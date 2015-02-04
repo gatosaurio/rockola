9588f4bdb435416c7d6dcfceb0a209c6c4f3b873
gatosaurio <javiervalente80@gmail.com>
2015-02-04 23:54:58 +0000

    primera version

---
+++ client/templates/songs/songs.js
@@ -0,0 +1,31 @@
+Meteor.subscribe('songs');
+
+Template.userSongs.helpers({
+  'songs': function(){
+     return Songs.find({})
+  },
+})
+
+Template.userSongs.events({
+  "submit form": function(event){
+    event.preventDefault();
+   /* var currentUserId = Meteor.userId();*/
+    var url  = event.target.url.value;
+    var urlMod = url.replace("watch?v=", "v/");
+   
+    
+   console.log(urlMod);
+    
+   Songs.insert({
+      urlMod: urlMod,
+      comment: "",
+      approved: false,
+      
+    });
+    
+    event.target.url.value = ""
+    
+    return false;
+    
+  }
+});
